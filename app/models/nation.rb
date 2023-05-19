72cf29253dc77327b72133ea52fae8774cf88d7a
esfalsa <pronoun.nationstates@gmail.com>
2023-05-18 17:08:36 -0700

    Create /nations/{name}/not_endorsed_by endpoint

--- app/models/nation.rb
+++ app/models/nation.rb
@@ -16,4 +16,12 @@ class Nation < ApplicationRecord
                                .where(region: Nation.find(target).region)
                                .order(:name)
                            }
+
+  scope :not_endorsing, lambda { |target|
+                          select(:name)
+                            .where('LOWER(name) NOT IN (?)', Nation.find(target).endorsements.map { |n| n.gsub("_"," ") })
+                            .where.not(unstatus: 'Non-member')
+                            .where(region: Nation.find(target).region)
+                            .order(:name)
+                        }
 end
